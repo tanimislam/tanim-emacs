20295d3ccef3691147fa0fe07d1509372ba3dbae
Tanim Islam <islam5@llnl.gov>
2020-04-22 00:08:38 -0700

    Changes to be committed: 	modified:   .gitignore 	modified:   custom.el 	modified:   init.el

--- custom.el
+++ custom.el
@@ -5,7 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
+<<<<<<< HEAD
     (ess zotelo zonokai-theme zenburn-theme yaml-mode windresize web-mode systemd swift3-mode swift-mode sphinx-mode sphinx-doc solarized-theme rjsx-mode python-mode poly-rst plain-theme pdf-tools pabbrev osx-plist nginx-mode multi-web-mode mmm-mode matlab-mode markdown-preview-mode magit lush-theme lua-mode leaf-convert latex-pretty-symbols landmark json-rpc json-mode jedi-direx jazz-theme ivy helm-cscope helm-chronos haskell-mode gotham-theme gitattributes-mode gist gandalf-theme fuzzy fm flymake-python-pyflakes f90-interface-browser exec-path-from-shell eldoc-eval ein dracula-theme danneskjold-theme context-coloring composer company-auctex company-anaconda color-theme-sanityinc-solarized cl-lib-highlight cl-generic chinese-fonts-setup charmap bts-github bibtex-utils base16-theme autumn-light-theme automargin atom-one-dark-theme angular-mode ample-theme all airline-themes ahungry-theme actionscript-mode ace-jump-mode ace-flyspell abyss-theme 4clojure))))
+=======
+    (ess zenburn-theme web-mode web-beautify tramp-theme systemd sphinx-mode sphinx-doc solarized-theme rust-mode polymode nginx-mode matlab-mode markdown-mode magit lush-theme lua-mode latex-unicode-math-mode latex-math-preview jedi jammer exwm exec-path-from-shell egg dracula-theme cycle-themes beacon auctex ace-window ace-jump-mode ace-jump-helm-line ace-flyspell))))
+>>>>>>> 4e0e801665dbbebeba2b72aca36a78452fbf9531
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
